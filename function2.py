6a66c5a03e9b096c9be16a81bc03f607e54461b7
jjshin0415 <visionwise01@gmail.com>
2024-04-15 16:35:08 +0900

    오늘 작업 완료

---
+++ function2.py
@@ -0,0 +1,14 @@
+#fucntion2.py
+#가변인자 처리
+def union(*ar): # 튜블 파라미터
+    #지역변수 초기화
+    result = []
+    for item in ar:
+        for x in item:
+            if x not in result:
+                result.append(x)
+    return result
+
+#여러가지 경우
+print(union("HAM","SPAM"))
+print(union("HAM","SPAM","EGG"))
