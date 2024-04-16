d673d05d3c86cc3fe1b1554f99447db23702f4b1
jjshin0415 <visionwise01@gmail.com>
2024-04-16 16:46:03 +0900

    데이터 입력추가

---
+++ db1.py
@@ -0,0 +1,26 @@
+#db1.py
+import sqlite3
+
+#일단 메모리에서 임시 저장
+con = sqlite3.connect(":memory:")
+
+#커서 객체
+cur = con.cursor()
+
+#테이블(자료구조) 생성
+cur.execute("create table PhoneBook (Name text, PhoneNum text);")
+
+#1건입력
+cur.execute("insert into PhoneBook values ('홍길동','010-222');")
+
+#외부에서 입력 파라미터 처리
+name = "전우치"
+phoneNum = "010-333"
+cur.execute("insert into PhoneBook values (?,?);",(name,phoneNum))
+
+#검색
+cur.execute("select * from PhoneBook;")
+
+for row in cur:
+     print(row)
+
